a4e7897920da22784cae410be7214d46aebfc9e0
vishwavegad <vegadvishwa17@gmail.com>
2025-04-15 13:26:15 +0530

    ui changes

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -38,10 +38,10 @@ function ContactMe() {
       <h1 className='font-bold text-3xl md:text-4xl text-center'>Contact Me</h1>
       <form onSubmit={onSubmit}>
         <div className="flex flex-col justify-center items-center mt-10 bg-[var(--card-background-color)] rounded-xl mx-auto shadow-md w-full max-w-xs sm:max-w-lg md:max-w-3xl lg:max-w-5xl">
-          <input type="text" placeholder="Name" name='name' required className="border rounded-2xl p-2 w-50 sm:w-100 m-2 mt-10 focus:outline-none"/>
-          <input type="text" placeholder="Email" name='email' required className="border rounded-2xl p-2 w-50 focus:outline-none"/>
-          <textarea name="message" placeholder="Message" className="border rounded-2xl p-2 h-50 w-50 m-2 focus:outline-none"></textarea>
-          <button className='rounded-xl p-2 w-50 mt-2 mb-10 hover:cursor-pointer bg-[var(--btn-color)] text-[var(--btn-text-color)] hover:bg-[var(--btn-hover-color)]'>Send</button>
+          <input type="text" placeholder="Name" name='name' required className="border rounded-2xl p-2 w-50 md:w-100 m-2 mt-10 focus:outline-none"/>
+          <input type="text" placeholder="Email" name='email' required className="border rounded-2xl p-2 w-50 md:w-100 focus:outline-none"/>
+          <textarea name="message" placeholder="Message" className="border rounded-2xl p-2 h-50 w-50 md:w-100 m-2 focus:outline-none"></textarea>
+          <button className='rounded-xl p-2 w-50 md:w-100 mt-2 mb-10 hover:cursor-pointer bg-[var(--btn-color)] text-[var(--btn-text-color)] hover:bg-[var(--btn-hover-color)]'>Send</button>
         </div>
       </form>
     </section>
